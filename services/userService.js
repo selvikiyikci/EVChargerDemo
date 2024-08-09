f30a70866974ee6c60bf8baae954df415df0bb83
selvikiyikci <selvikiyikci@gmail.com>
2024-08-09 13:07:24 +0300

    data operation functions

---
+++ services/userService.js
@@ -0,0 +1,49 @@
+module.exports = {
+    // Get all data
+    findAll: async function (model, options) {
+      return await model.findAll(options);
+    },
+  
+    // Find a specific data
+    findOne: async function (model, options) {
+      return await model.findOne(options);
+    },
+  
+    // Create a data
+    create: async function (model, options) {
+      return await model.create(options);
+    },
+  
+    // Find or created a data
+    findOrCreate: async function (model, options) {
+      const [data, created] = await model.findOrCreate(options);
+      const response = { data, created };
+      return response;
+    },
+  
+    findAndCountAll: async function (model, options) {
+      const { count, rows } = await model.findAndCountAll(options);
+      return { count, rows };
+    },
+  
+    countAll: async function (model, options) {
+      return await model.count(options);
+    },
+  
+    // Update a data
+    update: async function (model, updatedAttr, whereCondition) {
+      return await model.update(updatedAttr, whereCondition);
+    },
+  
+    // Delete a data
+    delete: async function (model, options, transaction) {
+      return await model.destroy(options, transaction);
+    },
+  
+    // Upsert a data
+    upsert: async function (model, options) {
+      const [data, created] = await model.upsert(options);
+      return data;
+    },
+  };
+  
