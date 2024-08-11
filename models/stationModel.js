f7a26fdd74dd5b1a07e1a37d7a4e38bfe3e150e1
selvikiyikci <selvikiyikci@gmail.com>
2024-08-11 14:15:44 +0300

    added data variables using sequelize

--- models/stationModel.js
+++ models/stationModel.js
@@ -1,33 +1,35 @@
 module.exports = (sequelize, Sequelize) => {
-    const station  = sequelize.define(
+    const station = sequelize.define(
         "station",
         {
-            chargePointID: {   
+            chargePointid: {   
+                type: Sequelize.STRING,
+                allowNull: false,
+                defaultValue: 'default_charge_point_id' // Varsayılan değer
+            },
+            name: {    
                 type: Sequelize.STRING,
                 allowNull: false
             },
-            areaID : {
-                type : Sequelize.STRING,
-                allowNull : false
-             },
-            name : {
+            latitude: {
+                type: Sequelize.FLOAT,
+                allowNull: false,
+            },
+            longitude: {
+                type: Sequelize.FLOAT,
+                allowNull: false,
+            },
+            distance: {
+                type: Sequelize.STRING,
+                allowNull: true
+            },
+            photoURL: {
                 type: Sequelize.STRING,
                 allowNull: false
-            },
-            address : {
-                type : Sequelize.STRING,
-                allowNull : false
-            },
-            city : {
-                type : Sequelize.STRING,
-                allowNull : false
-            },
-            district : {
-                type : Sequelize.STRING,
-                allowNull : false
             }
         },
         {
+            timestamps: false, // Burada 'timestaps' yerine 'timestamps' kullanmanız gerekir.
             freezeTableName: true
         }
     );
