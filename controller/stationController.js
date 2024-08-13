38d957304fa920cfda61e64d27d16c76f300d873
selvikiyikci <selvikiyikci@gmail.com>
2024-08-13 08:34:14 +0300

    added searchText and case sensivity

--- controller/stationController.js
+++ controller/stationController.js
@@ -1,114 +1,177 @@
 const { Op } = require("sequelize");
 const { stationModel } = require("../data/db.js");
-const userService = require("../services/userService.js");
+
 
 const haversineDistance = (lat1, lon1, lat2, lon2) => {
   const toRadians = (angle) => angle * (Math.PI / 180);
-  const R = 6371;
-  const dLat = toRadians(lat2 - lat1);
-  const dLon = toRadians(lon2 - lon1);
+  const R = 6371; 
+  const distanceLat = toRadians(lat2 - lat1);
+  const distanceLon = toRadians(lon2 - lon1);
   const a =
-    Math.sin(dLat / 2) ** 2 +
+    Math.sin(distanceLat / 2) ** 2 +
     Math.cos(toRadians(lat1)) *
       Math.cos(toRadians(lat2)) *
-      Math.sin(dLon / 2) ** 2;
+      Math.sin(distanceLon / 2) ** 2;
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   return R * c;
+};
+
+
+const getAcStationsWithSearchText = async (searchText, latRange, lonRange) => {
+  return await stationModel.findAll({
+    where: {
+      chargePointid: {
+        [Op.like]: "10%",
+      },
+      name: {
+        [Op.like]: `%${searchText}%`,
+      },
+      ...(latRange && lonRange
+        ? {
+            latitude: {
+              [Op.between]: latRange,
+            },
+            longitude: {
+              [Op.between]: lonRange,
+            },
+          }
+        : {}),
+    },
+   
+  });
+};
+
+
+const getAcStationsWithoutSearchText = async (latRange, lonRange) => {
+  return await stationModel.findAll({
+    where: {
+      chargePointid: {
+        [Op.like]: "10%",
+      },
+      ...(latRange && lonRange
+        ? {
+            latitude: {
+              [Op.between]: latRange,
+            },
+            longitude: {
+              [Op.between]: lonRange,
+            },
+          }
+        : {}),
+    },
+  });
+};
+
+
+const getDcStationsWithSearchText = async (searchText, latRange, lonRange) => {
+  return await stationModel.findAll({
+    where: {
+      chargePointid: {
+        [Op.like]: "11%",
+      },
+      name: {
+        [Op.like]: `%${searchText}%`,
+      },
+      ...(latRange && lonRange
+        ? {
+            latitude: {
+              [Op.between]: latRange,
+            },
+            longitude: {
+              [Op.between]: lonRange,
+            },
+          }
+        : {}),
+    },
+  });
+};
+  
+const getDcStationsWithoutSearchText = async (latRange, lonRange) => {
+  return await stationModel.findAll({
+    where: {
+      chargePointid: {
+        [Op.like]: "11%",
+      },
+      ...(latRange && lonRange
+        ? {
+            latitude: {
+              [Op.between]: latRange,
+            },
+            longitude: {
+              [Op.between]: lonRange,
+            },
+          }
+        : {}),
+    },
+  });
+};
+
+
+const calculateDistanceForStations = (stations, latitude, longitude) => {
+  return stations.map((station) => {
+    const distance = latitude && longitude ? haversineDistance(latitude,longitude,station.latitude,station.longitude ): null;
+
+    return {
+      chargePointid: station.chargePointid,
+      name: station.name,
+      latlng: {
+        latitude: station.latitude,
+        longitude: station.longitude,
+      },
+      distance: distance !== null ? distance.toFixed(1) : null,
+      photoUrl: station.photoURL,
+    };
+  });
 };
 
 module.exports = {
   listChargePoints: async (req, res) => {
     try {
-      const { latitude, longitude } = req.body;
+      const { latitude, longitude, searchText } = req.body;
       console.log("Girilen latitude bilgisi:", latitude);
       console.log("Girilen longitude bilgisi:", longitude);
-
-      const latRange = latitude ? [latitude - 0.1, latitude + 0.1] : undefined;
-      const lonRange = longitude ? [longitude - 0.1, longitude + 0.1] : undefined;
-
-      const acStations = await stationModel.findAll(userService, {
-        where: {
-          chargePointid: {
-            [Op.like]: "10%",
-          },
-          ...(latRange && lonRange
-            ? {
-                latitude: {
-                  [Op.between]: latRange,
-                },
-                longitude: {
-                  [Op.between]: lonRange,
-                },
-              }
-            : {}),
-        },
-      });
-      console.log("AC Stations found:", acStations);
-      // await userService.findOne(userModel,{
-      //  where: {
-      //  email: email
-      //}
-      // });
-      // DC istasyonları
-      const dcStations = await stationModel.findAll(userService, {
-        where: {
-          chargePointid: {
-            [Op.like]: "11%",
-          },
-          ...(latRange && lonRange
-            ? {
-                latitude: {
-                  [Op.between]: latRange,
-                },
-                longitude: {
-                  [Op.between]: lonRange,
-                },
-              }
-            : {}),
-        },
-      });
-      console.log("DC Stations found:", dcStations);
-
-      const calculateDistanceForStations = (stations) => {
-        return stations.map((station) => {
-          const distance =
-            latitude && longitude ? haversineDistance( latitude, longitude, station.latitude, station.longitude ) : null;
-          console.log(`Calculating distance for station: ${station.name}`);
-          console.log(`Distance: ${distance}`);
-
-          return {
-            chargePointid: station.chargePointid,
-            name: station.name,
-            latlng: {
-              latitude: station.latitude,
-              longitude: station.longitude,
-            },
-            distance: distance !== null ? distance.toFixed(1) : null,
-            photoUrl: station.photoURL,
-          };
-        });
-      };
-
-      const acStationsWithDistance = calculateDistanceForStations(acStations);
-      const dcStationsWithDistance = calculateDistanceForStations(dcStations);
-
-      
-      acStationsWithDistance.sort((a, b) => (a.distance-b.distance))
-      dcStationsWithDistance.sort((a, b) => (a.distance-b.distance))
+      console.log("Search Text:", searchText);
+      const latRange = latitude ? [parseFloat(latitude) - 0.5, parseFloat(latitude) + 0.5] : undefined;
+      const lonRange = longitude ? [parseFloat(longitude) - 0.5, parseFloat(longitude) + 0.5] : undefined;
+      const isSearchTextempty = !searchText || searchText.trim() === "";
+      const filterStations = (stations, searchText) => {
+  return stations.filter(station => 
+    station.name.toLowerCase().includes(searchText.toLowerCase())
+  );
+};
 
 
-      res.json({
-        "status" : "success",
-        "data" : {
+
+      if (isSearchTextempty) {
+        acStations = await getAcStationsWithoutSearchText(latRange, lonRange);
+        dcStations = await getDcStationsWithoutSearchText(latRange, lonRange);
+      } else {
+        acStations = await getAcStationsWithSearchText(searchText, latRange, lonRange);
+        acStations = filterStations(acStations, searchText);
+        dcStations = await getDcStationsWithSearchText(searchText, latRange, lonRange);
+        dcStations = filterStations(dcStations, searchText);
+      }
+   
+
+
+      const acStationsWithDistance = calculateDistanceForStations(acStations, latitude, longitude);
+      const dcStationsWithDistance = calculateDistanceForStations(dcStations, latitude, longitude);
+
+      acStationsWithDistance.sort((a, b) => (a.distance - b.distance));
+      dcStationsWithDistance.sort((a, b) => (a.distance - b.distance));
+
+      res.status(200).json({
+        "status": "success",
+        "data": {
           acStations: acStationsWithDistance,
-          dcStations: dcStationsWithDistance
-        }
+          dcStations: dcStationsWithDistance,
+        },
       });
     } catch (error) {
       console.error(error);
       res.status(500).json({
         success: false,
-        message: "Bir hata oluştu.",
+        message: "Error occurred",
       });
     }
   },
