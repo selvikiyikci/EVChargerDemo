6a113248e7a83b4994d53bb5272363af6ad2b2c0
selvikiyikci <selvikiyikci@gmail.com>
2024-08-14 17:45:03 +0300

    added a dns

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(function(err, req, res, next) {
 
 
   const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
+  app.listen(PORT,'0.0.0.0', () => {
       console.log(`Server is running on http://localhost:${PORT}`);
   });
 
