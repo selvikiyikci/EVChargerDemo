5d5411116351ef0c65b135c8130a228dea672dac
selvikiyikci <selvikiyikci@gmail.com>
2024-08-16 15:00:42 +0300

    added new attributes for the charging station

--- models/connectorModel.js
+++ models/connectorModel.js
@@ -1,20 +1,42 @@
+const stationModel = require("./stationModel");
+
 module.exports = (sequelize, Sequelize) => {
     const connector  = sequelize.define(
         "connector",
         {
-            connectorID: {    
-              
+            connectorID: {  
                 type: Sequelize.STRING,
                 allowNull: false
             },
-           
             status: {
                 type: Sequelize.ENUM,
                 values: ["Available", "Preparing", "Charging", "Finishing", "Unavailable"],
                 allowNull: false
+            },
+//
+            title : {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            power : {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            pricePerkWh: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            lat: {
+                type: Sequelize.FLOAT,
+                allowNull: false
+            },
+            long: {
+                type: Sequelize.STRING,
+                allowNull: false
             }
         },
         {
+            timestamps: false,
             freezeTableName: true
         }
     );
