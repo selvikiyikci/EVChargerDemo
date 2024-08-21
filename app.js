55c20d1024f1d7c4da47ed366bd18dbe62080d29
selvikiyikci <selvikiyikci@gmail.com>
2024-08-21 10:42:47 +0300

    deleted a row

--- app.js
+++ app.js
@@ -38,10 +38,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 
 
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT,'0.0.0.0', () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
-  });
+  // const PORT = process.env.PORT || 3000;
+  // app.listen(PORT,'0.0.0.0', () => {
+  //     console.log(`Server is running on http://localhost:${PORT}`);
+  // });
 
 });
 
