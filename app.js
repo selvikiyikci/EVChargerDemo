2ac2dac86c4e1ffdab4d1af4fa8de5894f1e36ab
selvikiyikci <selvikiyikci@gmail.com>
2024-08-22 11:35:38 +0300

    deleted a line

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 
 
-const app = express();
+  // const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+  // // view engine setup
+  // app.set('views', path.join(__dirname, 'views'));
+  // app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
